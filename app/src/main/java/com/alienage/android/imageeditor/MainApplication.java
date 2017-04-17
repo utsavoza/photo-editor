80a10ea59abae4eace47b06cd0d6d5d0f2dc8dd7
utsavoza <utsavoza96@gmail.com>
2017-04-18 03:02:39 +0530

    updated application

--- app/src/main/java/com/alienage/android/imageeditor/MainApplication.java
+++ app/src/main/java/com/alienage/android/imageeditor/MainApplication.java
@@ -11,9 +11,9 @@ import com.adobe.creativesdk.foundation.auth.IAdobeAuthClientCredentials;
 
 public class MainApplication extends Application implements IAdobeAuthClientCredentials {
 
-    private static final String CREATIVE_SDK_CLIENT_ID      = "56846827a2794ed6a4f1ca6928b3e764";
-    private static final String CREATIVE_SDK_CLIENT_SECRET  = "172b13b0-1971-4d48-b028-2ea8b2f1b827";
-    private static final String CREATIVE_SDK_REDIRECT_URI   = "ams+9b781ca8f76be3275af04c9af651fa84bfc9f4c1://adobeid/56846827a2794ed6a4f1ca6928b3e764";
+    private static final String CREATIVE_SDK_CLIENT_ID      = "<CLIENT ID>";
+    private static final String CREATIVE_SDK_CLIENT_SECRET  = "<CLIENT SECRET>";
+    private static final String CREATIVE_SDK_REDIRECT_URI   = "<REDIRECT URI>";
     private static final String[] CREATIVE_SDK_SCOPES = {"email", "profile", "address"};
 
     @Override
